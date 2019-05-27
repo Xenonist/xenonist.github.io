9d3f556177997fdeb5032b01c2158bb08afc4084
Xenonist <mihail.levin.1999@mail.ru>
2019-05-27 22:51:13 +0300

    10

--- js.js
+++ js.js
@@ -1,1 +1,18 @@
-console.log('done');
+function CheckPassword () {
+
+}
+
+function KeyReader (e) {
+	if (e.keyCode == 13) {
+		CheckPassword();
+	}
+}
+
+var mistakes_number = 0;
+var BTN = document.getElementById('BTN_check_pass');
+
+window.addEventListener("keydown", KeyReader);
+BTN.addEventListener("click", CheckPassword);
+
+// <div id = "hint_1">Попробуй написать на русском, с маленькой буквы и без пробелов =)</div>
+// <div id = "hint_2">Так, ладно. Это два слова, но запиши как одно. Больше не подскажу, пить меньше надо</div>
